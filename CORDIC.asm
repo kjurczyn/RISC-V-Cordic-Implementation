e59f9b6434cae73b3f43eeadb1bbc6fda71e100c
kjurczyn <01169180@pw.edu.pl>
2023-04-24 19:13:18 +0200

    small changes

--- CORDIC.asm
+++ CORDIC.asm
@@ -1,3 +1,14 @@
 #Implementation of CORDIC Algorithm in RISC-V Assembly using fixed-point arithmetic
 #Author: Konrad Jurczyñski-Chu
 #numbers - 32bit word
+#accepts angles from -90 to 90 
+#n = 32 rotations
+.data
+val_list: .word 
+pi: .word 
+angle: .word 
+sine: .word
+
+.text
+#step 1 - convert input to radians
+#step 2 - compute the sine/cosine
