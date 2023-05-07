29e20c011e6b34d8a629f689a7bff06039b5e0da
kjurczyn <01169180@pw.edu.pl>
2023-05-07 21:26:53 +0200

    Rewrote the whole damn thing

--- CORDIC.asm
+++ CORDIC.asm
@@ -1,51 +1,84 @@
 #Implementation of CORDIC Algorithm in RISC-V Assembly using fixed-point arithmetic
-#Author: Konrad Jurczy�ski-Chu
+#Author: Konrad Jurczynski-Chu
 #Input in pi radians [-1, 1)*pi
-#n = 32 rotations
+#n = 30 rotations
 .data
 ARCTAN_ARR: .word 0x0ee6b280,0x57f77910,0x2e7aabc7,0x1797f381,0x0bd7b23b,0x05ed5376,0x02f6d920,0x017b727d,0x00bdb9fc,0x005edd16,0x002f6e8d,0x0017b747,0x000bdba3,0x0005edd1,0x0002f6e8,0x00017b74,0x0000bdba,0x00005edd,0x00002f6e,0x000017b7,0x00000bdb,0x000005ed,0x000002f6,0x0000017b,0x000000bd,0x0000005e,0x0000002f,0x00000017,0x0000000b,0x00000005,0x00000002
-#PI: .word 
-N: 31 #(n-1)
+cos: .word 0x21e5a842 #x
+sin: .word 0 #y
 angle: .word 0x0 #45deg = 0.25 pi rad
+msg_sin: .asciz "SINUS\n"
+msg_cos: .asciz "COSINUS\n"
+newline: .asciz "\n"
 
 .text
-#step 1 - convert input to radians
-#step 2 - compute the sine/cosine
-	lw t1, angle
-	li t2, 0 #angles sum
-	la t3, ARCTAN_ARR # array end
-	addi t3, t3, 121
-	la t4, ARCTAN_ARR #array current
-	li a3, 0x80000000 #number to be added/subtracted to/from current x and y
-	li a4, 0 #x (cos)
-	li a5, 0 #y
-	
+#t5, t6 rejestry do operacji
+	li t0, 0 #loop counter
+	la t1, ARCTAN_ARR #array begin
 	
 	
 main_loop:
-	lw a6, (t4) #a6 = current atan angle
-	bgt t2, t1, subtract_angle
-	beq t2, t1, end
+	addi t0, t0, 1
+	lw t2, angle #t2 := current angle
+	srai t3, t2, 31 #t3 := bitmask
+	srli t5, t2, 31 #t5 := sign
 	
+	#computing new x
+	lw t4, sin #t4 := current y
+	lw t6, cos # t6 := current x
+	mv a3, t6 #a3 := current x
+	sra t4, t4, t0 # t4 := y/2^i
+	xor t4, t4, t3
+	add t4, t4, t5 # t4 := y/2^i * sign
+	sub t6, t6, t4 # t6 := x - y/2^i * sign
+	sw  t6, cos, a2 # store new x into memory
 	
-add_angle:
-	add t2, t2, a6
-	sub a4, a4, a3
-	add a5, a5, a3
-	b loop_end
-
-subtract_angle:
-	sub t2, t2, a6
-	add a4, a4, a3
-	sub a5, a5, a3
+	#computing new y
+	lw t4, sin # t4 := current y
+	mv t6, a3 #t6 := old(current) x
+	sra t6, t6, t0 # t6 := x/2^i
+	xor t6, t6, t3 
+	add t6, t6, t5 # t6 := x/2^i * sign
+	add t4, t4, t6 # t4 := y + x/2^i * sign
+	sw t4, sin, a2 # store new y into memory
 	
-loop_end:
-
-	srli a3, a3, 1
-	addi t4, t4, 4
-	blt t4, t3, main_loop
+	#computing new beta
+	lw t4, (t1) # t4 := arctan_arr[i]
+	xor t4, t4, t3
+	add t4, t4, t5 # t4 := sign * arctan_arr[i]
+	sub t2, t2, t4
+	sw t2, angle, a2
 	
-end:
-	li a7, 1
-	mv a0, a5
+	#loop end
+	addi t1, t1, 4
+	li t4, 32
+	blt t0, t4, main_loop
+#end main_loop
+	
+	li a7, 4
+	la a0, msg_sin
 	ecall
+	
+	li a7, 1
+	lw a0, sin
+	ecall
+	
+	li a7, 4
+	la a0, newline
+	ecall
+	
+	li a7, 4
+	la a0, msg_cos
+	ecall
+	
+	li a7, 1
+	lw a0, cos
+	ecall
+	
+	li a7, 10
+	ecall
+	
+	
+	
+	
+	
--- arctan_angles.py
+++ arctan_angles.py
@@ -2,7 +2,7 @@ from math import atan, pi
 
 y = 0
 x = atan(2**(-0))/pi
-print('0x'+'{:08x}'.format(int(x*(10**10)/10)), end=',')
+print('0x'+'{:08x}'.format(int(x*(10**9))), end=',')
 y+= 1
 for i in range(1, 31):
     x = atan(2**(-i))/pi
@@ -12,3 +12,8 @@ x = atan(2**(-31))/pi
 print('0x'+'{:08x}'.format(int(x*(10**10))))
 y+= 1
 print(y)
+a = 1
+for i in range(y):
+    a *= 1/((1+2**(-2*i)**1/2))
+print('0x'+'{:08x}'.format(int(a*(10**9))))
+print(a)
