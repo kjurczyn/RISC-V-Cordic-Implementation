dd4dbe59788d33b933841de81a8934f34abbceb3
kjurczyn <01169180@pw.edu.pl>
2023-05-08 17:33:38 +0200

    Some changes

--- CORDIC.asm
+++ CORDIC.asm
@@ -3,56 +3,63 @@
 #Input in pi radians [-1, 1)*pi
 #n = 30 rotations
 .data
-ARCTAN_ARR: .word 0x0ee6b280,0x57f77910,0x2e7aabc7,0x1797f381,0x0bd7b23b,0x05ed5376,0x02f6d920,0x017b727d,0x00bdb9fc,0x005edd16,0x002f6e8d,0x0017b747,0x000bdba3,0x0005edd1,0x0002f6e8,0x00017b74,0x0000bdba,0x00005edd,0x00002f6e,0x000017b7,0x00000bdb,0x000005ed,0x000002f6,0x0000017b,0x000000bd,0x0000005e,0x0000002f,0x00000017,0x0000000b,0x00000005,0x00000002
-cos: .word 0x21e5a842 #x
+ARCTAN_ARR: .word 0x3243F6A8, 0x1DAC6705, 0x0FADBAFC, 0x07F56EA6, 0x03FEAB76, 0x01FFD55B, 0x00FFFAAA, 0x007FFF55, 0x003FFFEA, 0x001FFFFD, 0x000FFFFF, 0x0007FFFF, 0x0003FFFF, 0x0001FFFF, 0x0000FFFF, 0x00007FFF, 0x00003FFF, 0x00001FFF, 0x00000FFF, 0x000007FF, 0x000003FF, 0x000001FF, 0x000000FF, 0x0000007F, 0x0000003F, 0x0000001F, 0x0000000F, 0x00000008, 0x00000004, 0x00000002, 0x00000001, 0x00000000
+cos: .word 0x26DD3B6A #x = k constant
 sin: .word 0 #y
-angle: .word 0x0 #45deg = 0.25 pi rad
+angle: .word 0x0 #45 deg
 msg_sin: .asciz "SINUS\n"
-msg_cos: .asciz "COSINUS\n"
-newline: .asciz "\n"
+msg_cos: .asciz "\nCOSINUS\n"
 
 .text
 #t5, t6 rejestry do operacji
-	li t0, 0 #loop counter
+	li t0, 1 #loop counter
 	la t1, ARCTAN_ARR #array begin
 	
-	
 main_loop:
+	lw t2, angle
+	lw t6, (t1)
+	bgtz t2, angle_positive
+angle_not_positive: #d = -1
+	add t2, t2, t6 #add angle
+	sw t2, angle, t4
+	#calculate new x
+	lw t4, sin #y
+	lw t5, cos #x
+	sra t4, t4, t0 #y/2^i
+	add t4, t4, t5   
+	sw t4, cos, t3 #save x
+	
+	#calculate new y
+	lw t4, sin #y, x is in t5
+	sra t5, t5, t0 #x/2^i
+	sub t5, t4, t5
+	sw t5, sin, t4 #save y 	
+	
+	b loop_end
+angle_positive: #d = 1
+	sub t2, t2, t6
+	sw t2, angle, t4
+	#calculate new x
+	lw t4, sin #y
+	lw t5, cos #x
+	sra t4, t4, t0 #y/2^i
+	sub t4, t5, t4   
+	sw t4, cos, t3 #save x
+	
+	#calculate new y
+	lw t4, sin #y, x is in t5
+	sra t5, t5, t0 #x/2^i
+	add t5, t4, t5
+	sw t5, sin, t4 #save y 	
+
+loop_end:
 	addi t0, t0, 1
-	lw t2, angle #t2 := current angle
-	srai t3, t2, 31 #t3 := bitmask
-	srli t5, t2, 31 #t5 := sign
-	
-	#computing new x
-	lw t4, sin #t4 := current y
-	lw t6, cos # t6 := current x
-	mv a3, t6 #a3 := current x
-	sra t4, t4, t0 # t4 := y/2^i
-	xor t4, t4, t3
-	add t4, t4, t5 # t4 := y/2^i * sign
-	sub t6, t6, t4 # t6 := x - y/2^i * sign
-	sw  t6, cos, a2 # store new x into memory
-	
-	#computing new y
-	lw t4, sin # t4 := current y
-	mv t6, a3 #t6 := old(current) x
-	sra t6, t6, t0 # t6 := x/2^i
-	xor t6, t6, t3 
-	add t6, t6, t5 # t6 := x/2^i * sign
-	add t4, t4, t6 # t4 := y + x/2^i * sign
-	sw t4, sin, a2 # store new y into memory
-	
-	#computing new beta
-	lw t4, (t1) # t4 := arctan_arr[i]
-	xor t4, t4, t3
-	add t4, t4, t5 # t4 := sign * arctan_arr[i]
-	sub t2, t2, t4
-	sw t2, angle, a2
-	
-	#loop end
 	addi t1, t1, 4
-	li t4, 32
-	blt t0, t4, main_loop
+	li t3, 32
+	bne t0, t3, main_loop
+	
+	
+end:
 #end main_loop
 	
 	li a7, 4
@@ -61,10 +68,6 @@ main_loop:
 	
 	li a7, 1
 	lw a0, sin
-	ecall
-	
-	li a7, 4
-	la a0, newline
 	ecall
 	
 	li a7, 4
--- arctan_angles.py
+++ arctan_angles.py
@@ -1,19 +1,22 @@
 from math import atan, pi
 
+mul = 1<<31
+
 y = 0
 x = atan(2**(-0))/pi
-print('0x'+'{:08x}'.format(int(x*(10**9))), end=',')
+print('0x'+'{:08x}'.format(int(x*mul)), end=',')
 y+= 1
 for i in range(1, 31):
     x = atan(2**(-i))/pi
-    print('0x'+'{:08x}'.format(int(x*(10**10))),end=',')
+    print('0x'+'{:08x}'.format(int(x*mul)),end=',')
     y+=1
 x = atan(2**(-31))/pi
-print('0x'+'{:08x}'.format(int(x*(10**10))))
+print('0x'+'{:08x}'.format(int(x*mul)))
 y+= 1
 print(y)
 a = 1
 for i in range(y):
     a *= 1/((1+2**(-2*i)**1/2))
-print('0x'+'{:08x}'.format(int(a*(10**9))))
+print('0x'+'{:08x}'.format(int(a*mul)))
 print(a)
+print(mul)
